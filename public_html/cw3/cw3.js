9e9b2f9042fe61ecf821e42e0ef41d825883ea16
cosinus <cosinus@cosinuskrakow>
2017-03-12 10:19:48 +0100

    first 1

---
+++ public_html/cw3/cw3.js
@@ -0,0 +1,14 @@
+
+window.onload = start;
+
+function start(){
+   var pars = document.getElementsByTagName("p");
+   alert(pars[1].innerHTML);
+   pars[4].innerHTML = "Fajnie tu jest";
+   pars[0].style.color = "red";
+   for(var i=0;i<pars.length;i++){
+       if(i%2!=0){
+           pars[i].style.backgroundColor = "yellow";
+       }
+   }
+}
