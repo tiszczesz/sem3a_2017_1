faa55b23cda57d1e7ac057f654dd7e1fe947cf32
cosinus <cosinus@cosinuskrakow>
2017-03-18 16:33:54 +0100

    first 1

--- public_html/cw6/cw6.js
+++ public_html/cw6/cw6.js
@@ -1,6 +1,25 @@
 
 window.onload = start;
-
+function getColors(){
+    return [
+        "grey",
+        "blue",
+        "green",
+        "yellow",
+        "red",
+        "purple"
+    ];
+}
+function getColors2(){
+    return [
+        ["grey","szary"],
+        ["blue","niebieski"],
+        ["green","zielony"],
+        ["yellow","zółty"],
+        ["red","czerwony"],
+        ["purple","fioletowy"]
+    ];
+}
 function start(){
     var radios = document.getElementsByName("wybor");
     document.getElementsByTagName("p")[0].style.fontSize = "12px";
