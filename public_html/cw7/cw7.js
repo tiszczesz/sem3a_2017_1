ce3b1e82395d9a7c2d0fef6a68400ed7dd8e6cb5
cosinus <cosinus@cosinuskrakow>
2017-03-19 11:33:26 +0100

    first 1

--- public_html/cw7/cw7.js
+++ public_html/cw7/cw7.js
@@ -1,6 +1,8 @@
 
 window.onload = start;
 var obrazki = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
+var kolory = ["white","green","yellow","grey","red"
+    ,"purple","#ff9999","#66ff66","#6Aff66","#12aff66"];
 var actual = 0;
 function start() {
     document.getElementById("obrazek").setAttribute("src", "images/"
@@ -9,6 +11,10 @@ function start() {
     document.getElementById("down").onclick = down;
     document.getElementById("up").onclick = up;
     setInterval(zmien,3000);
+    setInterval(function(){
+        var index = Math.floor(Math.random()*kolory.length);
+        document.body.style.backgroundColor = kolory[index];
+    },4000);
 }
 function zmien(){
     var index = Math.floor(Math.random()*obrazki.length);
