9d48a18b613efa95a22432609122b382c6f27f6e
cosinus <cosinus@cosinuskrakow>
2017-03-19 11:12:47 +0100

    first 1

--- public_html/cw7/cw7.js
+++ public_html/cw7/cw7.js
@@ -8,19 +8,27 @@ function start() {
     document.getElementById("podpis").innerHTML = obrazki[actual];
     document.getElementById("down").onclick = down;
     document.getElementById("up").onclick = up;
+    setInterval(zmien,3000);
+}
+function zmien(){
+    var index = Math.floor(Math.random()*obrazki.length);
+   // console.log(index);
+   document.getElementById("obrazek").setAttribute("src", "images/"
+            + obrazki[index]);
+    document.getElementById("podpis").innerHTML = obrazki[index];
 }
 function up() {
     actual++;
     actual = actual >= obrazki.length ? actual - obrazki.length : actual;
-    console.log(actual);
+    //console.log(actual);
     document.getElementById("obrazek").setAttribute("src", "images/"
             + obrazki[actual]);
     document.getElementById("podpis").innerHTML = obrazki[actual];
 }
-function down() {
+function down() {   
     actual--;
     actual = actual < 0 ? actual + obrazki.length : actual;
-    console.log(actual);
+   // console.log(actual);
     document.getElementById("obrazek").setAttribute("src", "images/"
             + obrazki[actual]);
     document.getElementById("podpis").innerHTML = obrazki[actual];
