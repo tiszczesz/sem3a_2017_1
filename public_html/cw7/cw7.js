8f5a6d6440855b3dae8f9c4189ab534f6cb002b5
cosinus <cosinus@cosinuskrakow>
2017-03-19 10:33:47 +0100

    first 1

--- public_html/cw7/cw7.js
+++ public_html/cw7/cw7.js
@@ -1,7 +1,27 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
+window.onload = start;
+var obrazki = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
+var actual = 0;
+function start() {
+    document.getElementById("obrazek").setAttribute("src", "images/"
+            + obrazki[actual]);
+    document.getElementById("podpis").innerHTML = obrazki[actual];
+    document.getElementById("down").onclick = down;
+    document.getElementById("up").onclick = up;
+}
+function up() {
+    actual++;
+    actual = actual >= obrazki.length ? actual - obrazki.length : actual;
+    console.log(actual);
+    document.getElementById("obrazek").setAttribute("src", "images/"
+            + obrazki[actual]);
+    document.getElementById("podpis").innerHTML = obrazki[actual];
+}
+function down() {
+    actual--;
+    actual = actual < 0 ? actual + obrazki.length : actual;
+    console.log(actual);
+    document.getElementById("obrazek").setAttribute("src", "images/"
+            + obrazki[actual]);
+    document.getElementById("podpis").innerHTML = obrazki[actual];
+}
